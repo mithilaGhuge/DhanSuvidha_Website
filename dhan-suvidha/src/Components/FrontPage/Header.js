a7ba9f3c1cfa88faed0f2d366f3bec172a08c08e
mithilaGhuge <mithila.ghuge@gmail.com>
2020-09-22 18:39:38 +0530

    Comming soon front page

---
+++ dhan-suvidha/src/Components/FrontPage/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import frontPage from './FrontPage.css';
+
+
+class HeaderBlock extends React.Component{
+    render(){
+        return(
+            <div class="frontPage">
+                <img src="./DhanSuviLogo1.png" Max-width= "100%" height="121"  alt="logo" />
+                <h2>COMMING SOON</h2>
+                <h3>Our Website Is Under Construction Now</h3>
+                <h4>We Are Working Very Hard To Give You The
+                Best Experience With This One
+                Feel Free To Reach Us On</h4>
+                <h5>Contact No : +91 9654620623         </h5>
+            </div>
+        );
+    }
+}
+
+export default HeaderBlock;
