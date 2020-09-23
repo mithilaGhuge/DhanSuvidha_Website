e798143c9fe808488ad0b60fb6b7bc04ff83dbc9
mithilaGhuge <mithila.ghuge@gmail.com>
2020-09-23 16:37:44 +0530

    Logo removed from coming soon page

--- dhan-suvidha/src/Components/FrontPage/Header.js
+++ dhan-suvidha/src/Components/FrontPage/Header.js
@@ -6,8 +6,8 @@ class HeaderBlock extends React.Component{
     render(){
         return(
             <div class="frontPage">
-                <img src="./DhanSuviLogo1.png" Max-width= "100%" height="121"  alt="logo" />
-                <h2>COMMING SOON</h2>
+                <h1>DHANSUVIDHA</h1>
+                <h2>COMING SOON</h2>
                 <h3>Our Website Is Under Construction Now</h3>
                 <h4>We Are Working Very Hard To Give You The
                 Best Experience With This One
