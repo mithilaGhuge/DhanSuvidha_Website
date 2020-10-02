b68fc6bc12df596be93e6cf3492d6c66670dc763
RohitKuDwivedi <rdwivedi345@gmail.com>
2020-10-02 14:44:15 +0530

    added footer

--- dhan-suvidha/src/Components/HeaderComponent.jsx
+++ dhan-suvidha/src/Components/HeaderComponent.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import "./HeaderComponent.css";
 import logo from "../images/dhanSuvidhaLogo.png";
-import { Container, Row } from "react-bootstrap";
 
 class HeaderComponent extends Component {
   state = {};
